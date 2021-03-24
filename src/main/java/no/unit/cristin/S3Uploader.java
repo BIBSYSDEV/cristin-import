87d33bd273a44650233fedf35461f07c72e56453
Orestis Gkorgkas <orestis.gkorgkas@unit.no>
2021-03-24 13:16:34 +0100

    default Constructor for Uploader

--- src/main/java/no/unit/cristin/S3Uploader.java
+++ src/main/java/no/unit/cristin/S3Uploader.java
@@ -4,13 +4,21 @@ import java.nio.file.Path;
 import java.util.List;
 import java.util.stream.Collectors;
 import no.unit.nva.s3.S3Driver;
+import nva.commons.core.Environment;
+import nva.commons.core.JacocoGenerated;
 import nva.commons.core.parallel.ParallelExecutionException;
 import nva.commons.core.parallel.ParallelMapper;
 
 public class S3Uploader {
 
     public static final int BATCH_SIZE = 100;
+    public static final String S3_BUCKET = "BUCKET";
     private final S3Driver s3Driver;
+
+    @JacocoGenerated
+    public S3Uploader() {
+        this(defaultS3Driver());
+    }
 
     public S3Uploader(S3Driver s3Driver) {
         this.s3Driver = s3Driver;
@@ -24,6 +32,12 @@ public class S3Uploader {
         ParallelMapper<KeyValue, String> mapper =
             new ParallelMapper<>(entries, this::uploadEntry, BATCH_SIZE).run();
         return collectFailedInsertionIds(mapper);
+    }
+
+    private static S3Driver defaultS3Driver() {
+        Environment environment = new Environment();
+        String bucketName = environment.readEnv(S3_BUCKET);
+        return S3Driver.fromPermanentCredentialsInEnvironment(bucketName);
     }
 
     private List<String> collectFailedInsertionIds(ParallelMapper<KeyValue, String> mapper) {
