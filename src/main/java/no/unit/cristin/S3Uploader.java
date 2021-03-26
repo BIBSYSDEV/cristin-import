09aafa8b840bcf802b10dadff591f9f0c99bd464
Orestis Gkorgkas <orestis.gkorgkas@unit.no>
2021-03-26 11:26:46 +0100

    test property setting

--- src/main/java/no/unit/cristin/S3Uploader.java
+++ src/main/java/no/unit/cristin/S3Uploader.java
@@ -1,37 +1,51 @@
 package no.unit.cristin;
 
+import static nva.commons.core.attempt.Try.attempt;
+import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.Properties;
 import java.util.stream.Collectors;
 import no.unit.nva.s3.S3Driver;
 import nva.commons.core.Environment;
 import nva.commons.core.JacocoGenerated;
+import nva.commons.core.ioutils.IoUtils;
 import nva.commons.core.parallel.ParallelExecutionException;
 import nva.commons.core.parallel.ParallelMapper;
 
 public class S3Uploader {
 
-    public static final int BATCH_SIZE = 100;
+    public static final String BATCH_SIZE_PROPERTY = "batchSize";
     public static final String S3_BUCKET = "BUCKET";
+    public static final String DEFAULT_PROPERTIES = "default.properties";
+    public static final String OVERRIDING_PROPERTIES_FILE_PATH = "PROPERTIES_PATH";
+
     private final S3Driver s3Driver;
+    private final Environment environment;
+    private final int batchSize;
+    private Properties applicationProperties;
 
     @JacocoGenerated
     public S3Uploader() {
-        this(defaultS3Driver());
+        this(defaultS3Driver(), new Environment());
     }
 
-    public S3Uploader(S3Driver s3Driver) {
+    public S3Uploader(S3Driver s3Driver, Environment environment) {
+        this.environment = environment;
         this.s3Driver = s3Driver;
+        attempt(this::initProperties).orElseThrow();
+        batchSize = Integer.parseInt(applicationProperties.getProperty(BATCH_SIZE_PROPERTY));
     }
 
     public void uploadFile(String entryId, String content) {
         s3Driver.insertFile(Path.of(entryId), content);
     }
 
-    public List<String> uploadFiles(List<KeyValue> entries) throws InterruptedException {
+    public List<KeyValue> uploadFiles(List<KeyValue> entries) throws InterruptedException {
         ParallelMapper<KeyValue, String> mapper =
-            new ParallelMapper<>(entries, this::uploadEntry, BATCH_SIZE).run();
-        return collectFailedInsertionIds(mapper);
+            new ParallelMapper<>(entries, this::uploadEntry, batchSize).run();
+        return collectFailedInsertionObjects(mapper);
     }
 
     @JacocoGenerated
@@ -41,11 +55,34 @@ public class S3Uploader {
         return S3Driver.fromPermanentCredentialsInEnvironment(bucketName);
     }
 
-    private List<String> collectFailedInsertionIds(ParallelMapper<KeyValue, String> mapper) {
+    private Void initProperties() throws IOException {
+        Properties defaultProperties = new Properties();
+        defaultProperties.load(IoUtils.inputStreamFromResources(DEFAULT_PROPERTIES));
+
+        Properties overridingProperties = readOverridingPropertiesFile();
+
+        applicationProperties = new Properties(defaultProperties);
+        applicationProperties.putAll(overridingProperties);
+        applicationProperties.putAll(System.getProperties());
+        return null;
+    }
+
+    private Properties readOverridingPropertiesFile() throws IOException {
+        InputStream overridingPropertiesContent = environment.readEnvOpt(OVERRIDING_PROPERTIES_FILE_PATH)
+                                                      .map(Path::of)
+                                                      .map(IoUtils::stringFromFile)
+                                                      .map(IoUtils::stringToStream)
+                                                      .orElse(InputStream.nullInputStream());
+
+        Properties overridingProperties = new Properties();
+        overridingProperties.load(overridingPropertiesContent);
+        return overridingProperties;
+    }
+
+    private List<KeyValue> collectFailedInsertionObjects(ParallelMapper<KeyValue, String> mapper) {
         return mapper.getExceptions().stream()
                    .map(ParallelExecutionException::getInput)
                    .map(inputObject -> (KeyValue) inputObject)
-                   .map(KeyValue::getKey)
                    .collect(Collectors.toList());
     }
 
